8eb7ca5b569856b0ca85a17c7b1b7cee24d11cc5
molot <molot@XOMIC-NOTEBOOK>
2022-02-20 12:48:22 +0700

    Добавьте файлы проекта.

---
+++ PracticaTask15/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PracticaTask15
+{
+    internal class Program
+    {
+        static String encryption(String text) {
+            String encryptText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    if (text[i] == 'я' || text[i] == 'Я')
+                    {
+                        encryptText += text[i] == 'я' ? "а" : "А";
+                    }
+                    else
+                    {
+                        if (text[i] != 'e' || text[i] != 'Е')
+                        {
+                            encryptText += (char)((int)(text[i]) + 1);
+                        }
+                        else
+                        {
+                            encryptText += (char)((int)(text[i]) + 2);
+                        }
+                    }
+                }
+                else
+                {
+                    encryptText += text[i];
+                }
+            }
+            return encryptText;
+        }
+        static String decryption(String text)
+        {
+            String decryptText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    if (text[i] == 'а' || text[i] == 'А')
+                    {
+                        decryptText += text[i] == 'а' ? "я" : "Я";
+                    }
+                    else
+                    {
+                        if (text[i] != 'e' || text[i] != 'Е')
+                        {
+                            decryptText += (char)((int)(text[i]) - 1);
+                        }
+                        else
+                        {
+                            decryptText += (char)((int)(text[i]) - 2);
+                        }
+                    }
+                }
+                else
+                {
+                    decryptText += text[i];
+                }
+            }
+            return decryptText;
+        }
+        static List<String> Spliting(String text) {
+            List<String> subStrings = new List<String>();
+            while(text != "")
+            {
+                subStrings.Add(text.Substring(text.IndexOf('(') +1, text.IndexOf('(') + text.IndexOf(')') - 1));
+                text = text.Remove(text.IndexOf('('), text.IndexOf('(') + text.IndexOf(')') + 1);
+            }
+            return subStrings;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку для шифрования: ");
+            String text = Console.ReadLine();
+            Console.WriteLine("Шифрованная строка: " + encryption(text));
+
+            Console.Write("Введите строку для дешифрования: ");
+            text = Console.ReadLine();
+            Console.WriteLine("Дешифрованная строка: " + decryption(text));
+            
+            Console.Write("Введите строку для разделения: ");
+            text = Console.ReadLine();
+            foreach (String s in Spliting(text)) {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
